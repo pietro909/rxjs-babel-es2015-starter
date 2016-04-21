67871120efb3defb23719c1aa9249d4c6922899e
Nader Dabit <dabit3@gmail.com>
2016-04-21 16:50:03 -0500

    Updated title

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
-console.log('hello world!')
+import Rx from 'rx'
+
+Rx.Observable.from([1, 2, 3, 4, 5])
+  .map((item) => {
+    return item + 1
+  })
+  .subscribe((x) => {
+    console.log('x: ', x)
+  })
