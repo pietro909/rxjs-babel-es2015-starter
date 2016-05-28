a503e72a986f0de010dad26b954cba6cf960e728
Pietro909 <pietro.grandi.3d@gmail.com>
2016-05-28 13:34:11 +0200

    test case for messages' mutability

--- app/app.js
+++ app/app.js
@@ -1,9 +1,44 @@
-import Rx from 'rx'
+import Rx from 'rx';
 
-Rx.Observable.from([1, 2, 3, 4, 5])
-  .map((item) => {
-    return item + 1
-  })
-  .subscribe((x) => {
-    console.log('x: ', x)
-  })
+const messages = Array(4).fill().map(() => {
+    const id = (Math.random()*1000).toString()
+        .replace(/\./,'').slice(0,8).split('')
+        .map((n) => String.fromCharCode(10+n).toUpperCase()).join('');
+    return {
+        id: id
+    };
+})
+
+// const observable = Rx.Observable.from(messages);
+const observable = Rx.Observable.interval(1000).timeInterval().map((interval) => {
+    const id = (Math.random()*1000).toString()
+        .replace(/\./,'').slice(0,8).split('')
+        .map((n) => String.fromCharCode(10+n).toUpperCase()).join('');
+    return {
+        id: id,
+        time: interval.value,
+        interval: interval.interval
+    };
+}).do((m) => {
+    console.log(`side effect with message ${m.id}`);
+}).share();
+
+const consumer1 = () => observable.subscribe((x) => {
+    console.log(x);
+    x.newProperty = "hi, I added a new property";
+});
+
+const consumer2 = () => observable.subscribe((x) => {
+    console.log(x);
+    x.id = 111222;
+});
+
+const consumer3 = () => observable.subscribe((x) => {
+    console.log(x);
+});
+
+consumer1();
+consumer2();
+consumer3();
+// setTimeout(consumer2, 1000);
+// setTimeout(consumer3, 2000);
